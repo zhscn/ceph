bc413ef28af6ce15e1e80c9ed3a60978b8ff788f
Zhang Song <zhangsong325@gmail.com>
2023-05-23 16:02:14 +0800

    crimson/os/seastore: avoid evicting promoted extents to the cold tier
    Signed-off-by: Zhang Song <zhangsong325@gmail.com>

--- src/crimson/os/seastore/transaction_manager.h
+++ src/crimson/os/seastore/transaction_manager.h
@@ -23,6 +23,7 @@
 #include "crimson/os/seastore/logging.h"
 #include "crimson/os/seastore/seastore_types.h"
 #include "crimson/os/seastore/cache.h"
+#include "crimson/os/seastore/cache/non_volatile_cache.h"
 #include "crimson/os/seastore/lba_manager.h"
 #include "crimson/os/seastore/backref_manager.h"
 #include "crimson/os/seastore/journal.h"
@@ -681,6 +682,10 @@ public:
     return epm->get_stat();
   }
 
+  bool support_non_volatile_cache() const {
+    return nv_cache != nullptr;
+  }
+
   ~TransactionManager();
 
 private:
@@ -693,6 +698,7 @@ private:
   BackrefManagerRef backref_manager;
 
   WritePipeline write_pipeline;
+  NonVolatileCache *nv_cache;
 
   rewrite_extent_ret rewrite_logical_extent(
     Transaction& t,
@@ -877,7 +883,6 @@ private:
 	std::move(ref->template cast<LogicalCachedExtent>()));
     });
   }
-
 
 public:
   // Testing interfaces
