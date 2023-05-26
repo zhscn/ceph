164b1746e1c5a5ee9599751ef633d5cdbcacba35
Zhang Song <zhangsong325@gmail.com>
2023-05-26 11:45:25 +0800

    max extent size

--- src/crimson/os/seastore/lba_manager.h
+++ src/crimson/os/seastore/lba_manager.h
@@ -128,6 +128,16 @@ public:
     retire_promotion_func_t retire_func,
     update_nextent_func_t update_func) = 0;
 
+  using alloc_extents_iertr = base_iertr;
+  using alloc_extents_ret = alloc_extents_iertr::future<lba_pin_list_t>;
+  virtual alloc_extents_ret alloc_extents(
+    Transaction &t,
+    laddr_t hint,
+    extent_len_t len,
+    paddr_t addr,
+    extent_len_t max_extent_size,
+    std::vector<LogicalCachedExtent*> nextents) = 0;
+
   struct ref_update_result_t {
     unsigned refcount = 0;
     paddr_t addr;
