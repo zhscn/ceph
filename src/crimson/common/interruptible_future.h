34436394e3b2f186ee13035d136f3f01f2a1b68c
Xuehan Xu <xuxuehan@qianxin.com>
2023-08-18 11:17:45 +0800

    crimson/common: add new log type "ceph_subsys_interrupt"
    Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

--- src/crimson/common/interruptible_future.h
+++ src/crimson/common/interruptible_future.h
@@ -11,7 +11,7 @@
 #include "crimson/common/log.h"
 #include "crimson/common/errorator.h"
 #ifndef NDEBUG
-#define INTR_FUT_DEBUG(FMT_MSG, ...) crimson::get_logger(ceph_subsys_).trace(FMT_MSG, ##__VA_ARGS__)
+#define INTR_FUT_DEBUG(FMT_MSG, ...) crimson::get_logger(ceph_subsys_interrupt).trace(FMT_MSG, ##__VA_ARGS__)
 #else
 #define INTR_FUT_DEBUG(FMT_MSG, ...)
 #endif
