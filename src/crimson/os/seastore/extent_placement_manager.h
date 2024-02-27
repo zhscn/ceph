9d04b3719ce407cb7e7e52e299dbb8172e61da66
Zhang Song <zhangsong02@qianxin.com>
2024-02-27 17:50:12 +0800

    crimson/os/seastore/EPM/BackgroundProcess: add promote and demote process
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>

--- src/crimson/os/seastore/extent_placement_manager.h
+++ src/crimson/os/seastore/extent_placement_manager.h
@@ -904,6 +904,15 @@ private:
 
     void maybe_wake_blocked_io() final;
 
+    void maybe_wake_promote() final {
+      if (!is_ready()) {
+        return;
+      }
+      if (memory_cache && memory_cache->should_promote()) {
+        do_wake_promote();
+      }
+    }
+
   private:
     // reserve helpers
     bool try_reserve_cold(std::size_t usage);
@@ -935,6 +944,13 @@ private:
       if (blocking_background) {
 	blocking_background->set_value();
 	blocking_background = std::nullopt;
+      }
+    }
+
+    void do_wake_promote() {
+      if (blocking_promote) {
+        blocking_promote->set_value();
+        blocking_promote = std::nullopt;
       }
     }
 
@@ -1093,6 +1109,8 @@ private:
 
     seastar::future<> do_background_cycle();
 
+    seastar::future<> run_promote();
+
     void register_metrics();
 
     struct {
@@ -1119,6 +1137,10 @@ private:
     std::optional<seastar::future<>> process_join;
     std::optional<seastar::promise<>> blocking_background;
     std::optional<seastar::promise<>> blocking_io;
+
+    std::optional<seastar::future<>> promote_process_join;
+    std::optional<seastar::promise<>> blocking_promote;
+
     bool is_running_until_halt = false;
     state_t state = state_t::STOP;
     eviction_state_t eviction_state;
