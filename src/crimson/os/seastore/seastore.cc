8980c552308c03bfe79d61b8e9ddd87048c55b5a
Zhang Song <zhangsong02@qianxin.com>
2024-02-27 17:50:54 +0800

    crimson/os/seastore: don't start background process during mount transaction manager
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>

--- src/crimson/os/seastore/seastore.cc
+++ src/crimson/os/seastore/seastore.cc
@@ -297,7 +297,9 @@ seastar::future<> SeaStore::Shard::mount_managers()
 {
   init_managers();
   return transaction_manager->mount(
-  ).handle_error(
+  ).safe_then([this] {
+    transaction_manager->start_background();
+  }).handle_error(
     crimson::ct_error::assert_all{
       "Invalid error in mount_managers"
   });
@@ -366,7 +368,10 @@ SeaStore::Shard::mkfs_managers()
   return transaction_manager->mkfs(
   ).safe_then([this] {
     init_managers();
-    return transaction_manager->mount();
+    return transaction_manager->mount(
+    ).safe_then([this] {
+      transaction_manager->start_background();
+    });
   }).safe_then([this] {
 
     ++(shard_stats.io_num);
