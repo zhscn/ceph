cdf0214d03104738686c29c3e5386cc1682d2b1a
Zhang Song <zhangsong02@qianxin.com>
2024-04-09 16:13:04 +0800

    crimson/os/seastore: init laddr_t with L_ADDR_NULL explicitly
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>

--- src/crimson/os/seastore/seastore_types.h
+++ src/crimson/os/seastore/seastore_types.h
@@ -1455,22 +1455,23 @@ inline uint64_t operator-(const loffset_t &lhs, const loffset_t &rhs) {
 }
 
 struct __attribute((packed)) laddr_le_t {
-  ceph_le64 laddr = ceph_le64(L_ADDR_NULL);
+  ceph_le64 low = ceph_le64(laddr_t::U64MAX);
+  ceph_le64 high = ceph_le64(laddr_t::U64MAX);
 
   using orig_type = laddr_t;
 
   laddr_le_t() = default;
   laddr_le_t(const laddr_le_t &) = default;
   explicit laddr_le_t(const laddr_t &addr)
-    : laddr(ceph_le64(addr)) {}
+    : low(static_cast<uint64_t>(addr.value)),
+      high(static_cast<uint64_t>(addr.value >> 64)) {}
 
   operator laddr_t() const {
-    return laddr_t(laddr);
+    return laddr_t(low, high);
   }
   laddr_le_t& operator=(laddr_t addr) {
-    ceph_le64 val;
-    val = addr;
-    laddr = val;
+    low = static_cast<uint64_t>(addr.value);
+    high = static_cast<uint64_t>(addr.value >> 64);
     return *this;
   }
 };
@@ -1560,9 +1561,9 @@ struct min_max_t {};
 
 template <>
 struct min_max_t<laddr_t> {
-  static constexpr laddr_t max = L_ADDR_MAX;
-  static constexpr laddr_t min = L_ADDR_MIN;
-  static constexpr laddr_t null = L_ADDR_NULL;
+  static constexpr laddr_t max = laddr_t::max();
+  static constexpr laddr_t min = laddr_t::min();
+  static constexpr laddr_t null = laddr_t::null();
 };
 
 template <>
@@ -1789,9 +1790,9 @@ get_average_time(const sea_time_point& t1, std::size_t n1,
 
 /* description of a new physical extent */
 struct extent_t {
-  extent_types_t type;  ///< type of extent
-  laddr_t addr;         ///< laddr of extent (L_ADDR_NULL for non-logical)
-  ceph::bufferlist bl;  ///< payload, bl.length() == length, aligned
+  extent_types_t type;         ///< type of extent
+  laddr_t addr = L_ADDR_NULL;  ///< laddr of extent (L_ADDR_NULL for non-logical)
+  ceph::bufferlist bl;         ///< payload, bl.length() == length, aligned
 };
 
 using extent_version_t = uint32_t;
