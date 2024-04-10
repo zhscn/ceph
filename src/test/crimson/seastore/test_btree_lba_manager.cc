afaabefc3e047b504b853ed3b02ffe7bbc9ad780
Zhang Song <zhangsong02@qianxin.com>
2024-04-10 13:59:50 +0800

    crimson/os/seastore: support alloc shadow mapping
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>

--- src/test/crimson/seastore/test_btree_lba_manager.cc
+++ src/test/crimson/seastore/test_btree_lba_manager.cc
@@ -290,7 +290,7 @@ struct lba_btree_test : btree_test_base {
   }
 
   static auto get_map_val(extent_len_t len) {
-    return lba_map_val_t{0, (pladdr_t)P_ADDR_NULL, len, 0};
+    return lba_map_val_t{0, pladdr_t(P_ADDR_NULL, /*has_shadow=*/false), len, 0};
   }
 
   device_off_t next_off = 0;
@@ -536,7 +536,8 @@ struct btree_lba_manager_test : btree_test_base {
 	  [this, &t, hint, determinsitic](auto &extents) {
 	  return lba_manager->alloc_extents(
 	    t, hint, std::move(extents),
-	    EXTENT_DEFAULT_REF_COUNT, determinsitic);
+	    EXTENT_DEFAULT_REF_COUNT,
+	    {.determinsitic=determinsitic});
 	});
       }).unsafe_get0();
     for (auto &ret : rets) {
