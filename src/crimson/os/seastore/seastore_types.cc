f6c3a9b1495e20e41a65abc110b270c6cb242f69
Zhang Song <zhangsong02@qianxin.com>
2024-06-03 15:29:42 +0800

    crimson/os/seastore: pladdr should only store local clone id instead the whole laddr
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>

--- src/crimson/os/seastore/seastore_types.cc
+++ src/crimson/os/seastore/seastore_types.cc
@@ -106,11 +106,15 @@ std::ostream &operator<<(std::ostream &out, const loffset_t &loffset)
 
 std::ostream &operator<<(std::ostream &out, const pladdr_t &pladdr)
 {
+  out << "pladdr(";
   if (pladdr.is_laddr()) {
-    return out << pladdr.get_laddr();
+    // pladdr(local_clone_id=...)
+    out << "local_clone_id=" << pladdr.get_local_clone_id();
   } else {
-    return out << pladdr.get_paddr();
+    // pladdr(paddr=<...>)
+    out << pladdr.get_paddr();
   }
+  return out << ")";
 }
 
 std::ostream &operator<<(std::ostream &out, const paddr_t &rhs)
