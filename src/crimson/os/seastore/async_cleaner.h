3625bc2179ec3ad7e0a6dd95b2e709643c6fb59d
Zhang Song <zhangsong02@qianxin.com>
2024-06-19 15:48:08 +0800

    crimson/os/seastore: introduce RoatationalDevice
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>

--- src/crimson/os/seastore/async_cleaner.h
+++ src/crimson/os/seastore/async_cleaner.h
@@ -1202,6 +1202,8 @@ public:
 
   virtual const std::set<device_id_t>& get_device_ids() const = 0;
 
+  virtual backend_type_t get_backend_type() const = 0;
+
   virtual std::size_t get_reclaim_size_per_cycle() const = 0;
 
 #ifdef UNIT_TESTS_BUILT
@@ -1394,6 +1396,10 @@ public:
 
   const std::set<device_id_t>& get_device_ids() const final {
     return sm_group->get_device_ids();
+  }
+
+  backend_type_t get_backend_type() const final {
+    return backend_type_t::SEGMENTED;
   }
 
   std::size_t get_reclaim_size_per_cycle() const final {
@@ -1723,6 +1729,10 @@ public:
 
   const std::set<device_id_t>& get_device_ids() const final {
     return rb_group->get_device_ids();
+  }
+
+  backend_type_t get_backend_type() const final {
+    return backend_type_t::RANDOM_BLOCK;
   }
 
   std::size_t get_reclaim_size_per_cycle() const final {
