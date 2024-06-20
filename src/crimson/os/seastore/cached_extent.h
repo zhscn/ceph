22be8f896ab75826af91f33a4e721af3e4d1284b
Zhang Song <zhangsong02@qianxin.com>
2024-06-20 19:30:16 +0800

    crimson/os/seastore: support promote extents purged from memory cache
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>

--- src/crimson/os/seastore/cached_extent.h
+++ src/crimson/os/seastore/cached_extent.h
@@ -651,6 +651,15 @@ public:
   void reclaim_paddr() {
     poffset_reclaimed = true;
   }
+
+  uint8_t get_cache_state() const {
+    return cache_state;
+  }
+
+  void set_cache_state(uint8_t state) {
+    cache_state = state;
+  }
+
 private:
   template <typename T>
   friend class read_set_item_t;
@@ -751,6 +760,9 @@ private:
   // We add this field which is only set when the extent is mutable and
   // is remapped to tell Cache not mark the extent's space free
   bool poffset_reclaimed = false;
+
+  // opaque state of CachedExtent, used by MemoryCache
+  uint8_t cache_state = 0;
 
 protected:
   trans_view_set_t mutation_pendings;
