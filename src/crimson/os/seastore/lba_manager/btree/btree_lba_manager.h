4fa02ae9544e40f3f74ba0f7b6abb39db7a61f64
Zhang Song <zhangsong02@qianxin.com>
2024-07-17 15:38:55 +0800

    crimson/os/seastore: introduce shadow paddr in lba mapping
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>

--- src/crimson/os/seastore/lba_manager/btree/btree_lba_manager.h
+++ src/crimson/os/seastore/lba_manager/btree/btree_lba_manager.h
@@ -208,6 +208,14 @@ public:
       *p.find_pending_version(ctx.trans, get_key()));
     return viewable_p.get_mapping(ctx, get_key());
   }
+
+  bool has_shadow_mapping() const final {
+    return !is_indirect() && map_val.shadow_paddr != P_ADDR_NULL;
+  }
+
+  paddr_t get_shadow_val() const final {
+    return map_val.shadow_paddr;
+  }
 protected:
   std::unique_ptr<BtreeNodeMapping<laddr_t, paddr_t>> _duplicate(
     op_context_t<laddr_t> ctx) const final {
@@ -298,7 +306,8 @@ public:
 	  len,
 	  pladdr_t(P_ADDR_ZERO),
 	  EXTENT_DEFAULT_REF_COUNT,
-	  0
+	  0,
+	  P_ADDR_NULL
 	},
 	nullptr
       };
@@ -313,8 +322,9 @@ public:
 	  len,
 	  pladdr_t(intermediate_key),
 	  EXTENT_DEFAULT_REF_COUNT,
-	  0	// crc will only be used and checked with LBA direct mappings
+	  0,    // crc will only be used and checked with LBA direct mappings
 		// also see pin_to_extent(_by_type)
+	  P_ADDR_NULL
 	},
 	nullptr
       };
@@ -325,7 +335,8 @@ public:
       paddr_t paddr,
       extent_ref_count_t refcount,
       uint32_t checksum,
-      LogicalCachedExtent *extent) {
+      LogicalCachedExtent *extent,
+      paddr_t shadow_paddr) {
       return {
 	laddr,
 	lba_map_val_t{
@@ -333,6 +344,7 @@ public:
 	  pladdr_t(paddr),
 	  refcount,
 	  checksum,
+	  shadow_paddr
 	},
 	extent
       };
@@ -414,7 +426,8 @@ public:
 	ext.get_paddr(),
 	refcount,
 	ext.get_last_committed_crc(),
-	&ext)};
+	&ext,
+	P_ADDR_NULL)};
     return seastar::do_with(
       std::move(alloc_infos),
       [this, &t, hint, determinsitic](auto &alloc_infos) {
@@ -447,7 +460,8 @@ public:
 	  extent->get_paddr(),
 	  refcount,
 	  extent->get_last_committed_crc(),
-	  extent.get()));
+	  extent.get(),
+	  P_ADDR_NULL));
     }
     return seastar::do_with(
       std::move(alloc_infos),
@@ -567,12 +581,29 @@ public:
 	      std::move(mappings));
 	  });
 	} else { // !orig_mapping->is_indirect()
-	  fut = alloc_extents(
-	    t,
-	    remaps.front().offset + orig_laddr,
-	    std::move(extents),
-	    EXTENT_DEFAULT_REF_COUNT,
-	    true);
+	  assert(remaps.size() == extents.size());
+	  std::vector<alloc_mapping_info_t> alloc_infos;
+	  for (auto i = 0; i < extents.size(); i++) {
+	    auto &extent = extents[i];
+	    auto &remap = remaps[i];
+	    assert(extent->has_laddr());
+	    assert(extent->get_laddr() - orig_mapping->get_key() == remap.offset);
+	    assert(extent->get_length() == remap.len);
+	    alloc_infos.emplace_back(alloc_mapping_info_t::create_direct(
+	      extent->get_laddr(),
+	      extent->get_length(),
+	      extent->get_paddr(),
+	      EXTENT_DEFAULT_REF_COUNT,
+	      extent->get_last_committed_crc(),
+	      extent.get(),
+	      orig_mapping->has_shadow_mapping()
+	      ? orig_mapping->get_shadow_val().add_offset(remap.offset)
+	      : P_ADDR_NULL));
+	  }
+	  auto hint = remaps.front().offset + orig_laddr;
+	  fut = seastar::do_with(std::move(alloc_infos), [this, &t, hint](auto &infos) {
+	    return _alloc_extents(t, hint, infos, true);
+	  });
 	}
 
 	return fut.si_then([&ret, &remaps, &orig_mapping](auto &&refs) {
@@ -667,6 +698,7 @@ private:
     lba_pin_list_t res;
     std::vector<alloc_mapping_info_t> mappings;
     std::list<LogicalCachedExtentRef> remapped_extents;
+    std::list<LogicalCachedExtentRef> remapped_shadow_extents;
 
     laddr_t get_src_end() const { return src_base + length; }
 
