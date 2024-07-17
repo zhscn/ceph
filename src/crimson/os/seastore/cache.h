308938bf67948f32c24f0009521fc07229a6d3a7
Zhang Song <zhangsong02@qianxin.com>
2024-07-17 15:40:57 +0800

    crimson/os/seastore: implement promote extent
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>

--- src/crimson/os/seastore/cache.h
+++ src/crimson/os/seastore/cache.h
@@ -984,6 +984,15 @@ public:
     return extent;
   }
 
+  CachedExtentRef alloc_remapped_extent_by_type(
+    Transaction &t,
+    extent_types_t type,
+    laddr_t remap_laddr,
+    paddr_t remap_paddr,
+    extent_len_t remap_offset,
+    extent_len_t remap_length,
+    const std::optional<ceph::bufferptr> &original_bptr);
+
   /**
    * alloc_new_extent
    *
@@ -1567,6 +1576,7 @@ private:
 
     version_stat_t committed_dirty_version;
     version_stat_t committed_reclaim_version;
+    version_stat_t committed_promote_version;
 
     uint64_t write_hit_hot;
     uint64_t write_hit_cold;
