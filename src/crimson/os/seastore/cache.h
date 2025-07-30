68819ddb3af67ab68055574f1e78cc6efdc89415
Zhang Song <zhangsong02@qianxin.com>
2025-07-30 15:39:55 +0800

    crimson/os/seastore: don't add shadow extent to cache
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>

--- src/crimson/os/seastore/cache.h
+++ src/crimson/os/seastore/cache.h
@@ -1581,6 +1581,9 @@ private:
       extent_len_t load_length)
   {
     assert(ext.get_paddr().is_absolute());
+    if (ext.is_shadow_extent()) {
+      return;
+    }
     if (hint == CACHE_HINT_NOCACHE && is_logical_type(ext.get_type())) {
       return;
     }
