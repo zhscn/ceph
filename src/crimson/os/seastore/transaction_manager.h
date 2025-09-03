f7317b16a001a44bcc2bfc8304ea64174ddc13a1
Zhang Song <zhangsong02@qianxin.com>
2025-09-03 16:04:12 +0800

    crimson/os/seastore: avoid evicting promoted extent to the cold tier
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>

--- src/crimson/os/seastore/transaction_manager.h
+++ src/crimson/os/seastore/transaction_manager.h
@@ -26,6 +26,7 @@
 #include "crimson/os/seastore/logging.h"
 #include "crimson/os/seastore/seastore_types.h"
 #include "crimson/os/seastore/cache.h"
+#include "crimson/os/seastore/logical_bucket.h"
 #include "crimson/os/seastore/root_meta.h"
 #include "crimson/os/seastore/lba_manager.h"
 #include "crimson/os/seastore/backref_manager.h"
@@ -1195,6 +1196,10 @@ public:
     co_return mapping;
   }
 
+  bool support_logical_bucket() const {
+    return logical_bucket != nullptr;
+  }
+
   ~TransactionManager();
 
 private:
@@ -1207,6 +1212,7 @@ private:
   BackrefManagerRef backref_manager;
 
   WritePipeline write_pipeline;
+  LogicalBucket *logical_bucket;
 
   bool full_extent_integrity_check = true;
 
